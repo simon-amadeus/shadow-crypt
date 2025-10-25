c176afe9e7c9d41968a777fb4456e1cb87514f67
Simon Amadeus <simon.joedicke@code.berlin>
2025-10-25 12:43:14 +0200

    refactor: remove dead code

--- src/lib.rs
+++
@@ -1,8 +0,0 @@
-//! Shadow Crypt - Secure file encryption with obfuscation
-//!
-//! This crate provides secure file encryption with optional filename obfuscation.
-//! Built with a functional core / imperative shell architecture.
-
-// Simple re-export of the main functionality
-// pub use shadow_core::*;
-// pub use shadow_shell::*;
